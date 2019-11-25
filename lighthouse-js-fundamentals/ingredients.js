288245041a087c7203d8b13b8db02feb5a27225f
Anisa <anisahamohamed@gmail.com>
2019-11-25 23:09:29 +0000

    initial commit

--- lighthouse-js-fundamentals/ingredients.js
+++ lighthouse-js-fundamentals/ingredients.js
@@ -1,17 +1,36 @@
-const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+// const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
-// Write a while loop that prints out the contents of ingredients:
-var i=0;
- while(i < ingredients.length){
-     console.log(ingredients[i]);
-     i++;
-};
-// Write a for loop that prints out the contents of ingredients:
-for (var i = 0; i < ingredients.length;  i++) {
-console.log(ingredients[i]);
-};
+// // Write a while loop that prints out the contents of ingredients:
+// var i=0;
+//  while(i < ingredients.length){
+//      console.log(ingredients[i]);
+//      i++;
+// };
+// // Write a for loop that prints out the contents of ingredients:
+// for (var i = 0; i < ingredients.length;  i++) {
+// console.log(ingredients[i]);
+// };
 
-// Write any loop (while or for) that prints out the contents of ingredients backwards:
-for (var i =ingredients.length - 1; i >= 0; i--) {
-console.log(ingredients[i]);
+// // Write any loop (while or for) that prints out the contents of ingredients backwards:
+// for (var i =ingredients.length - 1; i >= 0; i--) {
+// console.log(ingredients[i]);
+// };
+
+const chooseStations = (stations) => {
+  // Code here!
+  let final = [];
+for (let i = 0; i < stations.length; i++){
+  if (stations[i][1] > 20 && (stations[i][2] ==='community centre' || stations[i][2] === 'school' )){
+    final.push(stations[i][0]);
+  }
+}
+  console.log(final);
 };
+  // Remember to return a value!
+
+    const stations = [
+      ['Big Bear Donair', 10, 'restaurant'],
+      ['Bright Lights Elementary', 50, 'school'],
+      ['Moose Mountain Community Centre', 45, 'community centre']
+    ];
+    chooseStations(stations)
--- lighthouse-js-fundamentals/packing-list.js
+++ lighthouse-js-fundamentals/packing-list.js
@@ -1,8 +1,30 @@
-console.log("Kitchen stuff to pack:");
-let i = 0;
-while (i < packingList.length) {
-  console.log(packingList[i]);
-  i++;
-}
+// console.log("Kitchen stuff to pack:");
+// let i = 0;
+// while (i < packingList.length) {
+//   console.log(packingList[i]);
+//   i++;
+// }
 
 
+// range(0, 10, 2);	[ 0, 2, 4, 6, 8, 10 ]
+// range(10, 30, 5);	[ 10, 15, 20, 25, 30 ]
+// range(-5, 2, 3);	[ -5, -2, 1 ]
+// // The function should return an empty array [] if given incorrect parameters, such as:
+// start, end, or step being undefined
+// start being greater than end
+// step being 0, or negative
+
+let final = [];
+
+let range = (start, end, step) => {
+	if(step === undefined || end === undefined||step === undefined || start>end||step<=0){
+		console.log(final);
+	};
+
+	while ( (start + step) <= (end+step)) {
+		final.push(start);
+		start = start + step;
+	}
+	console.log(final);
+};
+range(0, 10, 2)
